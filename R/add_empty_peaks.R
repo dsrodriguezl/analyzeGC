a3f490c3dd325ba690822133530b31e4d8339c56
dsrodriguezl <72925497+dsrodriguezl@users.noreply.github.com>
2023-03-10 23:22:20 +0100

    new function to add empty peaks (rows) to an aligned data frame

---
+++ R/add_empty_peaks.R
@@ -0,0 +1,41 @@
+#' @title
+#'
+#' @description
+#'
+#' @param aligned_df
+#' @param empty.peaks
+#'
+#' @export
+add_empty_peaks <- function(aligned_df, empty.peaks) {
+
+  if (nrow(aligned_df) > 1) {
+    # Loop iterating through samples for the alignment correction
+    for (sample in empty.peaks |> names()) {
+
+      if (sample %in% row.names(aligned_df)) {
+        cat('\n')
+        print(sample)
+        df <- empty.peaks[[sample]]
+        for (row in row.names(df) |> as.integer()) {
+          print(row)
+          new_peak <- paste(df$direction[row]
+                            , df$position.reference[row]
+                            , sep = "_")
+          print(new_peak)
+
+          if (df$direction[row] == "before") {
+            aligned_df <- aligned_df |>
+              add_column("{new_peak}" := 0
+                         , .before = df$position.reference[row])
+          }
+          if (df$direction[row] == "after") {
+            aligned_df <- aligned_df |>
+              add_column("{new_peak}" := 0
+                         , .after = df$position.reference[row])
+          }
+        }
+      }
+    }
+  }
+  aligned_df
+}
