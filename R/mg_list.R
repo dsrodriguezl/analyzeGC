54e050b0c5452d6056bb9d4d2112ab5d3153401c
Daniel Rodriguez <72925497+dsrodriguezl@users.noreply.github.com>
2023-03-10 12:47:20 +0100

    edit function

--- R/mg_list.R
+++ R/mg_list.R
@@ -3,18 +3,19 @@
 #' @description A function to Group samples gcms integration data frames based
 #' on unique group labels, within the list containing the data frames.
 #'
-#' @param sample.info
+#' @param sample.info A data frame with a column
+#' @param group.label
 #' @param samples.data.list
 #'
 #' @import dplyr
 #' @import purrr
 #'
 #' @export
-mg_list <- function(sample.info, samples.data.list){
+mg_list <- function(sample.info, group.label, samples.data.list){
   mg_list <- list()
-  for (i in unique(sample.info$group_label)) {
+  for (i in unique(sample.info[[group.label]])) {
     tmp <- sample.info |>
-      filter(group_label == i) |>
+      filter(get(group.label) == i) |>
       pull(`Individual`)
     tmp <- keep(samples.data.list
                 , samples.data.list |>
