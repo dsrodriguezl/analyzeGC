452dbd43fa633cf78c2de11ed3c58afec34570e7
Daniel Rodriguez <72925497+dsrodriguezl@users.noreply.github.com>
2023-03-13 17:54:34 +0100

    Documenting the package

--- R/get_chc_info.R
+++ R/get_chc_info.R
@@ -1,23 +1,55 @@
-#' @title
+#' @title get the CHC information data frame
 #'
-#' @description
+#' @description Function to generate the data frame holding the compound
+#' information for the hydrocarbons present in each sample, within a table
 #'
 #' @param comps.id
+#' data frame with the compound information for the hydrocarbons found in each
+#' sample.
+#'
+#' The data frame should have a **"Compound"** and a **"mean_RT"** columns, plus a
+#' column for every sample.
+#'
+#' The Compound column should have the name of the hydrocarbons with the
+#' following format:
+#' C*numeric chain length* _ *class abbreviation* _
+#' *numeric position of unsaturations / methyl-branches*.
+#'
+#' As an example the entry for the compound 15-Me C20 would be C20_Me_15-.
+#'
+#' The function automatically extracts the information of the compound from the
+#' entry in the **"Compound"** column, and replaces it by the correct name.
+#'
+#' If a peak contains more than one compound with unsaturations /
+#' methyl-branches, they must be separated by a ";" (e.g. C20_Me_13-;15-
+#' for a peak containing both 13-Me C20 and 15-Me C20).
+#'
+#'
+#' ## Class abbreviations are accepted by the function:
+#' n-alkanes: ane
+#' alkene: ene
+#' alkadiene: diene
+#' monomethyl-branched alkanes: Me
+#' Dimethyl-branched alkanes: Dime
+#' Trimethyl-branched alkanes: Trime
+#'
+#' The rest of the data frame, is just as it can be obtained via the [RT_df()]
+#' function, or after alignment correction.
 #'
 #' @export
 get_chc_info <- function(comps.id) {
   # Extract compounds information from the comps table
   group.comps.info <- comps.id |>
-    select(Peak:Compound) |>
+    select(contains("Peak"):contains("Compound")) |>
     cbind(data.frame(row.names = rownames(comps.id)
                      , t(
                        as.data.frame(
                          strsplit(comps.id$Compound
                                   , "_")
+                         )
                        )
                      )
-    )
-    ) |>
+          ) |>
     as_tibble()
 
   # cat('\n')
@@ -25,10 +57,10 @@ get_chc_info <- function(comps.id) {
   # cat('\n')
 
   # Define columns' names
-  colnames(group.comps.info) <- c(colnames(comps.id |>
-                                             select(Peak:Compound)
-                                           )
-                                  , "Chain.length", "Class", "Mod.position")
+  colnames(group.comps.info) <-
+    c(colnames(comps.id |>
+                 select(contains("Peak"):contains("Compound")))
+      , "Chain.length", "Class", "Mod.position")
   # cat('\n')
   # print("The columns of the comps_info data frame were renamed")
   # cat('\n')
@@ -47,13 +79,13 @@ get_chc_info <- function(comps.id) {
   ### of the alkanes)
   comps.names[!is.na(comps.names)] <-
     paste0(group.comps.info |>
-             filter(!is.na(Compound)) |>
+             filter(!is.na(get("Compound"))) |>
              pull("Mod.position")
            , group.comps.info |>
-             filter(!is.na(Compound)) |>
+             filter(!is.na(get("Compound"))) |>
              pull("Class")
            , group.comps.info |>
-             filter(!is.na(Compound)) |>
+             filter(!is.na(get("Compound"))) |>
              pull("Chain.length")) |>
     str_replace_all(paste(c("ane", "ene", "diene")
                           , collapse = '|')
@@ -64,10 +96,12 @@ get_chc_info <- function(comps.id) {
   comps.names[!is.na(comps.names)] <-
     paste(comps.names[!is.na(comps.names)]
           , group.comps.info |>
-            filter(!is.na(Compound)) |>
-            pull(Class) |>
-            str_replace(paste(c("ane", "Me"
-                                , "Dime", "Trime")
+            filter(!is.na(get("Compound"))) |>
+            pull("Class") |>
+            str_replace(paste(c("ane"
+                                , "Me"
+                                , "Dime"
+                                , "Trime")
                               , collapse = '|')
                         , "NA")
           , sep = ":") |>
@@ -99,10 +133,10 @@ get_chc_info <- function(comps.id) {
     "Alkadiene"
   group.comps.info['Class'][group.comps.info['Class'] == "Me"] <-
     "Methyl"
-  #  group.comps.info['Class'][group.comps.info['Class'] == "Dime"] <-
-  #    "Dimethyl"
-  #  group.comps.info['Class'][group.comps.info['Class'] == "Trime"] <-
-  #    "Trimethyl"
+  group.comps.info['Class'][group.comps.info['Class'] == "Dime"] <-
+     "Dimethyl"
+   group.comps.info['Class'][group.comps.info['Class'] == "Trime"] <-
+     "Trimethyl"
   #  group.comps.info['Class'][group.comps.info['Class'] == "Tetrame"] <-
   #    "Tetramethyl"
 
--- data-raw/data sets.R
+++ data-raw/data sets.R
@@ -1,11 +1,13 @@
 ## code to prepare `data sets` dataset goes here
 
-# load the fucntions of the package
+library(here)
+# load the functions of the package
 load_all()
 
 # Samples_data_list ----
 # Import the CSV files with the samples'integration results
-samples_path_data <- list.files(path = system.file("extdata"
+samples_path_data <- list.files(path = system.file(here("extdata"
+                                                        , "gcms_integration")
                                                    , package = "analyzeGC")
                                 #  Get all CSV files in the folder
                                 , pattern = ".CSV|.csv"
@@ -20,7 +22,7 @@ use_data(samples_data_list, overwrite = TRUE)
 
 # Samples_info ----
 # Load the samples list
-samples_info  <- here::here("data-raw", "samples-list.csv") |>
+samples_info  <- here("data-raw", "samples-list.csv") |>
   readr::read_csv()
 
 # Create the samples_info data file for the package
