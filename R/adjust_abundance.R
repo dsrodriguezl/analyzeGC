4d5996774ea282774cd0f476425cb68189c21e3e
dsrodriguezl <72925497+dsrodriguezl@users.noreply.github.com>
2023-03-17 11:42:28 +0100

    Document the package

--- R/adjust_abundance.R
+++ R/adjust_abundance.R
@@ -1,55 +1,104 @@
-#' @title Adjust the abundance of the peaks within samples
+#' @title Adjust abundance values within samples and display diagnostic plots
 #'
-#' @param aligned_data Aligned data set, as obtained with [recalculate_meanRT]
+#' @param aligned_data Aligned data set, as obtained with [recalculate_meanRT].
+#'
 #' @param std.info Data frame with the standards information, as obtained
-#' from [shape_hcstd_info]
+#' from [shape_hcstd_info].
+#' It must contains columns "mean_RT" and "area_correction".
+#'
+#' @import dplyr
+#' @import tidyr
+#' @import tibble
+#'
+#' @returns A list containing two elements: a data frame with adjusted area
+#' values and a data frame containing the RT values.
+#' It also prints bar plots representing both the un-corrected and corrected
+#' abundance values of each sample, using ggplot2.
+#'
+#' @examples
+#'
+#' # Adjusting the abundance for a single data set
+#' IW_data <- corrected_samples_list2$`Winter_In-hive workers_A. m. mellifera`
+#'   adjusted_IW <- adjust_area_and_plot(aligned_data = IW_data
+#'                                         , std_info = std_info)
+#'
+#' # Adjusting the abundance for several data sets within a list
+#' filtered_samples_list <- unfiltered_samples_list |>
+#'   lapply(trace_comps, threshold = 0.01)
 #'
 #' @export
 adjust_abundance <- function(aligned_data, std.info) {
 
   area_table <- aligned_data$Area
 
+  # Define a function that takes an input value x and divides it by a correction
+  # factor
   adjust.area <- function(x, correction =  area.adjustment) (x / correction)
 
   area_table2 <- tibble()
+
+  # Loop over each row in std.info, except for the last row
   for (i in 1:(nrow(std.info) - 1)) {
+    # Extract the mean_RT values defining the RT interval within which
+    # to correct the abundance of peaks in the current iteration
     interval <- std.info$mean_RT[i:(i + 1)]
 
+    # If this is the first iteration of the loop
     if (i == 1) {
+      # Extract the area correction values for compounds that eluted before
+      # the first standard
       area.adjustment <- std.info |>
-        filter(mean_RT == min(interval)) |>
-        pull(area_correction)
+        filter(get("mean_RT") == min(interval)) |>
+        pull("area_correction")
 
+      # Adjust the abundance of peaks, within every sample, with RT <= mean_Rt
+      # of the first standard
       tmp <- area_table |>
-        filter(mean_RT <= min(interval)) |>
+        filter(get("mean_RT") <= min(interval)) |>
         mutate_at(area_table |>
-                    select(-(Peak:mean_RT)) |>
+                    select(-(contains("Peak"):contains("mean_RT"))) |>
                     colnames()
                   , adjust.area)
+
+      # Add the adjusted rows to area_table2
       area_table2 <-  area_table2 |>
         bind_rows(tmp)
     }
 
+    # Extract the area correction values for compounds that eluted before
+    # the standard with the highest mean_RT in the current iteration
+    # and after the standard with the lowest mean_RT in the current iteration
     area.adjustment <- std.info |>
-      filter(mean_RT == max(interval)) |>
-      pull(area_correction)
+      filter(get("mean_RT") == max(interval)) |>
+      pull("area_correction")
 
+    # Adjust the abundance of peaks, within every sample, with RT <= mean_Rt
+    # of the standard with the highest mean_RT and RT > mean_RT with the lowest
+    # mean_RT
     tmp <- area_table |>
-      filter(min(interval) < mean_RT & mean_RT <= max(interval)) |>
+      filter(min(interval) < get("mean_RT") &
+               get("mean_RT") <= max(interval)) |>
       mutate_at(area_table |>
-                  select(-(Peak:mean_RT)) |>
+                  select(-(contains("Peak"):contains("mean_RT"))) |>
                   colnames()
                 , adjust.area)
+
+    # Add the adjusted rows to area_table2
     area_table2 <-  area_table2 |>
       bind_rows(tmp)
 
+    # If this is the last iteration of the loop
     if (i == (nrow(std.info) - 1)) {
+      # Adjust the abundance of peaks, within every sample, with RT > mean_Rt
+      # of the last standard
       tmp <- area_table |>
-        filter(max(interval) <  mean_RT) |>
+        filter(max(interval) <  get("mean_RT")) |>
         mutate_at(area_table |>
-                    select(-(Peak:mean_RT)) |>
+                    select(-(contains("Peak"):get("mean_RT"))) |>
                     colnames()
                   , adjust.area)
+
+      # Add the adjusted rows to area_table2
       area_table2 <-  area_table2 |>
         bind_rows(tmp)
     }
@@ -57,30 +106,34 @@ adjust_abundance <- function(aligned_data, std.info) {
   corrected_area_table <- area_table2
 
   area_table <- area_table |>
-    pivot_longer(-(Peak:mean_RT)
+    pivot_longer(-(contains("Peak"):contains("mean_RT"))
                  , names_to = "sample"
                  , values_to = "area")
 
   area_table2 <- area_table2 |>
-    pivot_longer(-(Peak:mean_RT)
+    pivot_longer(-(contains("Peak"):contains("mean_RT"))
                  , names_to = "sample"
                  , values_to = "corrected_area")
 
   area_table <-  area_table |>
     bind_cols(corrected_area = area_table2$corrected_area)
 
+  # Loop over each sample
   for (muestra in unique(area_table$sample)) {
     long_sample_table <- area_table |>
-      pivot_longer(-(Peak:sample)
+      pivot_longer(-(contains("Peak"):contains("sample"))
                    , names_to = "ab_type"
                    , values_to = "abundance") |>
-      filter(sample == muestra)
+      filter(get("sample") == muestra)
 
+    # Create a bar plot showing the abundance on y-axis and mean_RT on x-axis
+    # Use facet the plot, to show the un-adjusted and adjusted versions of the
+    # peaks' abundance within the sample defined by the current iteration
     p <- long_sample_table |>
-      ggplot(aes(y = abundance
-                 , x = mean_RT)) +
+      ggplot(aes(y = get("abundance")
+                 , x = get("mean_RT"))) +
       geom_col(color = "black") +
-      facet_wrap(vars(ab_type)
+      facet_wrap(vars(get("ab_type"))
                  , ncol = 1
                  , scales = "free_y") +
       theme_classic() +
@@ -90,5 +143,7 @@ adjust_abundance <- function(aligned_data, std.info) {
     print(p)
   }
 
-  return(corrected_area_table)
+  aligned_data[["Area"]] <- corrected_area_table
+
+  return(aligned_data)
 }
