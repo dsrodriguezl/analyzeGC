4ac0855dea28a8b3e438bfe4ae7d9c1a0bc5ac25
dsrodriguezl <72925497+dsrodriguezl@users.noreply.github.com>
2023-03-18 20:00:55 +0100

    The scale transformation works properly now

--- R/abundance_transformation.R
+++ R/abundance_transformation.R
@@ -1,39 +1,69 @@
-#' Transform a numeric master_daten frame
+#' @title Transform peak abundance values in a master table
 #'
-#' This function takes a master table and transforms the peak abundance values
-#' within each sample, based on the specified transformation option.
+#' @description This function takes a master table and transforms the peak
+#' abundance values within each sample, based on the specified transformation
+#' option.
 #'
 #' @param master.table A master table, for which the abundance values should be
 #' transformed.
 #'
-#' @param transformation The type of transformation to perform.
-#' Can be one of "percentage", "proportion", or "scale".
-#' Defaults to "percentage".
+#' @param transformation TA character string specifying the type of
+#' transformation to perform. Possible values: "percentage" (default),
+#' "proportion", or "scale".
 #'
-#' @param internal_standard_peak A character specifying the Peak (e.g. "P2") that
-#' contians the internal standard added to the samples, to be used
-#' `transformation` is set to "scale".
+#' @param internal_standard_peak A character string specifying the Peak
+#' (e.g., "P2") containing the internal standard added to the samples,
+#' to be used when the `transformation` argument is set to "scale".
 #'
-#' @param internal_standard_amount A numeric value, indicating the known amount
-#' of internal standard that was added to the samples.
+#' @param internal_standard_amount A numeric value indicating the known amount
+#' of internal standard added to the samples.
+#'
+#'@param calibration_plot A logical value indicating whether to generate
+#' calibration plots for each sample column when `transformation` is set to
+#' "scale".
 #'
 #' @import dplyr
 #' @import purrr
+#' @import ggplot2
+#'
+#' @examples
+#'
+#' # Percentage transformation
+#' master_table_transformed <-
+#' abundance_transformation(master_table_reassembled)
+#'
+#' # Proportion transformation
+#' master_table_transformed <-
+#' abundance_transformation(master_table_reassembled
+#'                          , transformation = "proportion")
+#'
+#' # Scale transformation
+#' master_table_transformed <-
+#' abundance_transformation(master_table_reassembled
+#'                          , transformation = "scale"
+#'                          , internal_standard_peak = "P4"
+#'                          , internal_standard_amount = 250
+#'                          , calibration_plot = F)
 #'
 abundance_transformation <- function(master.table
                                      , transformation = c("percentage"
                                                           , "proportion"
                                                           , "scale")
                                      , internal_standard_peak = NULL
-                                     , internal_standard_amount = NULL) {
+                                     , internal_standard_amount = NULL
+                                     , calibration_plot = T) {
   # Match the transformation argument to one of the allowed options
   transformation <- match.arg(transformation)
 
+  # Select only the columns that contain the abundance values of the peaks for
+  # each sample
   master_daten <- master.table |>
     select_if(is.double)
 
+  # Replace any NA values with 0
   master_daten[is.na(master_daten)] <- 0
 
+  # Select the columns containing the information of the compunds in the peaks
   compound_variables <- master.table |>
     select(-all_of(colnames(master_daten)))
 
@@ -83,9 +113,41 @@ abundance_transformation <- function(master.table
                           , y = c(0, internal_standard_amount)) |>
                  # Use the lm() function to fit a linear model of y as a
                  # function of sample
-                 lm(y ~ sample
+                 lm(y ~ 0 + sample
                     , data = _)
     })
+
+    if (calibration_plot == T) {
+      create_plot <- function(model, index) {
+        # Get the slope and intercept from the model
+        slope <- coef(model)
+
+        # Get the two points used to generate the model
+        data <- data.frame(
+          sample = c(0, model$model$sample[2]),
+          y = c(0, model$model$y[2])
+        )
+
+        # Create the ggplot with an abline and the two points, and include the
+        # index in the title
+        ggplot(data, aes(x = get("sample"), y = get("y"))) +
+          geom_point(size = 3) +
+          geom_abline(slope = slope
+                      , intercept = 0
+                      , color = "blue") +
+          labs(x = "Sample Value"
+               , y = "Internal Standard Amount"
+               , title = paste("Calibration curve for sample", index)) +
+          theme_classic()
+      }
+
+      # Use map2() to create a list of ggplots, one for each model, with the
+      # corresponding index included in the title
+      calibration_plots <- map2(calibration_curves
+                                , names(calibration_curves)
+                                , create_plot)
+      print(calibration_plots)
+    }
 
     # Use predict() to transform each column based on its corresponding
     # calibration curve
@@ -95,76 +157,6 @@ abundance_transformation <- function(master.table
                                             , data.frame(sample = .x))) |>
       # Replace any negative value with 0
       map(~ ifelse(. < 0, 0, .))
-      # map2(master_daten
-      #      , calibration_curves
-      #      , ~ {
-      #        # Use the predict() function to transform the values of the
-      #        # current sample based on the corresponding calibration curve
-      #        predict(.y
-      #                , as.data.frame(.x))
-      #      })
-
-    # Plot calibration curves and transformed samples
-    calibration_plots <-
-      map2(calibration_curves
-           , names(calibration_curves)
-           , ~ {
-             # Create a scatter plot of the calibration data points (0, 0) and
-             # (internal_standard_amount, corresponding sample value) using red
-             # points. Add a blue line to the plot that represents the linear
-             # regression line of the calibration curve. Set the title and
-             # subtitle of the plot to indicate that it is a calibration curve
-             # and to indicate the sample name
-             ggplot() +
-               geom_point(data = data.frame(sample = c(0
-                                                       , .x$data$sample[2])
-                                            ,
-                                            y = c(0
-                                                  , internal_standard_amount)
-                                            )
-                          , aes(sample, y)
-                          , color = "red") +
-               geom_smooth(aes(sample
-                               , y)
-                           , method = "lm"
-                           , data = .x
-                           , color = "blue") +
-               labs(title = "Calibration Curve"
-                    , subtitle = .y
-                    , x = "Sample"
-                    , y = "Amount")
-           })
-
-    transformed_plots <-
-      map2(transformed_samples
-           , names(transformed_samples)
-           , ~ {
-             # Create a scatter plot of the transformed data points
-             # using black points. Set the title and subtitle of the plot to
-             # indicate that it is a transformed column and to indicate the
-             # sample name
-             ggplot() +
-               geom_point(data = data.frame(sample = master_daten[[.y]]
-                                            , y = internal_standard_amount)
-                          , aes(sample
-                                , y)
-                          , color = "black") +
-               labs(title = "Transformed Sample"
-                    , subtitle = .y
-                    , x = "Sample"
-                    , y = "Amount")
-           })
-
-    # FOR TRIALS
-    # pdf(here::here("data-raw"
-    #                , 'calibration_curves.pdf'))
-    # calibration_plots
-    # dev.off()
-    #
-    # pdf(here::here("data-raw"
-    #                , 'transformed_samples.pdf'))
-    # transformed_plots
-    # dev.off()
 
     # Combine transformed columns into a single master_daten frame
     master_daten <- as.data.frame(transformed_samples)
